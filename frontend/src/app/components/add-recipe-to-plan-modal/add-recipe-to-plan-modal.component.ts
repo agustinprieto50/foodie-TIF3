92249d67ea2b209c33d90e6e54ac36b8daa062dd
Agustin Prieto <agustinprieto@Agustins-MBP.lan>
2023-05-02 17:36:01 -0300

    recipes

--- frontend/src/app/components/add-recipe-to-plan-modal/add-recipe-to-plan-modal.component.ts
+++ frontend/src/app/components/add-recipe-to-plan-modal/add-recipe-to-plan-modal.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { GetPlansService } from 'src/app/services/get-plans.service';
+import { Observable } from 'rxjs';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-recipe-to-plan-modal',
   templateUrl: './add-recipe-to-plan-modal.component.html',
   styleUrls: ['./add-recipe-to-plan-modal.component.css']
 })
-export class AddRecipeToPlanModalComponent {
+export class AddRecipeToPlanModalComponent implements OnInit{
+  plans$!: Observable<any[]>;
+  selectedPlan:number = 0;
+  
+  constructor(private getPlans: GetPlansService){
 
+  }
+
+  ngOnInit(){
+    this.plans$ = this.getPlans.getPlans()
+  }
+
+  onSelectPlan(planId:number){
+    this.selectedPlan = planId
+  }
+
+  onSubmit(){
+    
+  }
 }
